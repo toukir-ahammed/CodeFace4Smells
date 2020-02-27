0ebdfa91e7b596a1adbf99bc5dc4ef83969d7104
Toukir Ahammed <bsse0806@iit.du.ac.bd>
2020-02-27 15:02:13 +0600

    integration scripts updated

--- integration-scripts/install_cppstats.sh
+++ integration-scripts/install_cppstats.sh
@@ -14,7 +14,7 @@ echo '#!/bin/bash' > $CPPSTATS/cppstats
 echo "cd $CPPSTATS" >> $CPPSTATS/cppstats
 echo "PYTHONPATH=\"\$PYTHONPATH:$CPPSTATS/lib\" ./cppstats.py \"\$@\"" >> $CPPSTATS/cppstats
 chmod +x $CPPSTATS/cppstats
-wget --quiet http://sdml.info/lmcrs/srcML-Ubuntu12.04-64.tar.gz -O /tmp/srcML.tar.gz
+wget --quiet http://131.123.42.38/lmcrs/beta/srcML-Ubuntu14.04-64.tar.gz -O /tmp/srcML.tar.gz
 tar -xvf /tmp/srcML.tar.gz
 cp -rf $PWD/srcML/* $CPPSTATS/lib/srcml/linux/
 
